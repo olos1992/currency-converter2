2442d921e67cb4206b94f9a7734ebbce377ae793
Aleksander Skula <aleksander.skula@gmail.com>
2024-02-12 18:10:18 +0100

    if else changed to switch

--- js/script.js
+++ js/script.js
@@ -12,23 +12,34 @@ const outputUSD = document.querySelector(".js-outputUSD");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (inputPLN.checked && outputPLN.checked) {
-        result.innerText = `${amount.value} PLN`
-    } else if (inputPLN.checked && outputEUR.checked) {
-        result.innerText = `${(amount.value / 4.34).toFixed(2)} EUR`
-    } else if (inputPLN.checked && outputUSD.checked) {
-        result.innerText = `${(amount.value / 4.03).toFixed(2)} USD`
-    } else if (inputEUR.checked && outputPLN.checked) {
-        result.innerText = `${(amount.value * 4.34).toFixed(2)} PLN`
-    } else if (inputEUR.checked && outputEUR.checked) {
-        result.innerText = `${amount.value} EUR`
-    } else if (inputEUR.checked && outputUSD.checked) {
-        result.innerText = `${(amount.value * 1.077).toFixed(2)} USD`
-    } else if (inputUSD.checked && outputPLN.checked) {
-        result.innerText = `${(amount.value * 4.03).toFixed(2)} PLN`
-    } else if (inputUSD.checked && outputEUR.checked) {
-        result.innerText = `${(amount.value * 0.929).toFixed(2)} EUR`
-    } else if (inputUSD.checked && outputUSD.checked) {
-        result.innerText = `${amount.value} USD`
-    }
+
+    switch (true) {
+        case inputPLN.checked && outputPLN.checked:
+            result.innerText = `${(amount.value / 1).toFixed(2)} PLN`;
+            break;
+        case inputPLN.checked && outputEUR.checked:
+            result.innerText = `${(amount.value / 4.34).toFixed(2)} EUR`;
+            break;
+        case inputPLN.checked && outputUSD.checked:
+            result.innerText = `${(amount.value / 4.03).toFixed(2)} USD`;
+            break;
+        case inputEUR.checked && outputPLN.checked:
+            result.innerText = `${(amount.value * 4.34).toFixed(2)} PLN`;
+            break;
+        case inputEUR.checked && outputEUR.checked:
+            result.innerText = `${(amount.value / 1).toFixed(2)} EUR`;
+            break;
+        case inputEUR.checked && outputUSD.checked:
+            result.innerText = `${(amount.value * 1.077).toFixed(2)} USD`;
+            break;
+        case inputUSD.checked && outputPLN.checked:
+            result.innerText = `${(amount.value * 4.03).toFixed(2)} PLN`;
+            break;
+        case inputUSD.checked && outputEUR.checked:
+            result.innerText = `${(amount.value * 0.929).toFixed(2)} EUR`;
+            break;
+        case inputUSD.checked && outputUSD.checked:
+            result.innerText = `${(amount.value / 1).toFixed(2)} USD`;
+            break;
+    };
 });
