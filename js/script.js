793a1c3c155be636a46a023189b322c5e1f1de67
Aleksander Skula <aleksander.skula@gmail.com>
2024-02-19 06:12:33 +0100

    js syntax changed to 4 functions

--- js/script.js
+++ js/script.js
@@ -1,44 +1,54 @@
-const form = document.querySelector(".js-form");
-const amount = document.querySelector(".js-amount");
-const result = document.querySelector(".js-result");
+{
+    const calculateResult = (amount) => {
+        const inputPLN = document.querySelector(".js-inputPLN");
+        const inputEUR = document.querySelector(".js-inputEUR");
+        const inputUSD = document.querySelector(".js-inputUSD");
 
-const inputPLN = document.querySelector(".js-inputPLN");
-const inputEUR = document.querySelector(".js-inputEUR");
-const inputUSD = document.querySelector(".js-inputUSD");
+        const outputPLN = document.querySelector(".js-outputPLN");
+        const outputEUR = document.querySelector(".js-outputEUR");
+        const outputUSD = document.querySelector(".js-outputUSD");
 
-const outputPLN = document.querySelector(".js-outputPLN");
-const outputEUR = document.querySelector(".js-outputEUR");
-const outputUSD = document.querySelector(".js-outputUSD");
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    switch (true) {
-        case inputPLN.checked && outputPLN.checked:
-            result.innerText = `${(amount.value / 1).toFixed(2)} PLN`;
-            break;
-        case inputPLN.checked && outputEUR.checked:
-            result.innerText = `${(amount.value / 4.34).toFixed(2)} EUR`;
-            break;
-        case inputPLN.checked && outputUSD.checked:
-            result.innerText = `${(amount.value / 4.03).toFixed(2)} USD`;
-            break;
-        case inputEUR.checked && outputPLN.checked:
-            result.innerText = `${(amount.value * 4.34).toFixed(2)} PLN`;
-            break;
-        case inputEUR.checked && outputEUR.checked:
-            result.innerText = `${(amount.value / 1).toFixed(2)} EUR`;
-            break;
-        case inputEUR.checked && outputUSD.checked:
-            result.innerText = `${(amount.value * 1.077).toFixed(2)} USD`;
-            break;
-        case inputUSD.checked && outputPLN.checked:
-            result.innerText = `${(amount.value * 4.03).toFixed(2)} PLN`;
-            break;
-        case inputUSD.checked && outputEUR.checked:
-            result.innerText = `${(amount.value * 0.929).toFixed(2)} EUR`;
-            break;
-        case inputUSD.checked && outputUSD.checked:
-            result.innerText = `${(amount.value / 1).toFixed(2)} USD`;
-            break;
+        switch (true) {
+            case inputPLN.checked && outputPLN.checked:
+                return `${(amount.value / 1).toFixed(2)} PLN`;
+            case inputPLN.checked && outputEUR.checked:
+                return `${(amount.value / 4.34).toFixed(2)} EUR`;
+            case inputPLN.checked && outputUSD.checked:
+                return `${(amount.value / 4.03).toFixed(2)} USD`;
+            case inputEUR.checked && outputPLN.checked:
+                return `${(amount.value * 4.34).toFixed(2)} PLN`;
+            case inputEUR.checked && outputEUR.checked:
+                return `${(amount.value / 1).toFixed(2)} EUR`;
+            case inputEUR.checked && outputUSD.checked:
+                return `${(amount.value * 1.077).toFixed(2)} USD`;
+            case inputUSD.checked && outputPLN.checked:
+                return `${(amount.value * 4.03).toFixed(2)} PLN`;
+            case inputUSD.checked && outputEUR.checked:
+                return `${(amount.value * 0.929).toFixed(2)} EUR`;
+            case inputUSD.checked && outputUSD.checked:
+                return `${(amount.value / 1).toFixed(2)} USD`;
+        }
     };
-});
+
+    const updateResultText = (amount, result) => {
+        result.innerText = calculateResult(amount);
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amount = document.querySelector(".js-amount");
+        const result = document.querySelector(".js-result");
+
+        updateResultText(amount, result);
+    };
+
+    const init = () => {
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+
+}
